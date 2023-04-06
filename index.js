c5f454e9d847e4978be42b6b045ed48031b683d8
Vlad <vladus007445@gmail.com>
2023-04-06 21:31:00 +0300

    home work #6

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-const firstNumber = Number(prompt('Введите первое число:')) 
-const secondNumber = Number(prompt('Введите второе число:')) 
+const numberOfHours = Number(prompt('Введіть кількість годин:'))
+const numberOfSeconds = numberOfHours * 3600
 
-alert(`${firstNumber} + ${secondNumber} = ${firstNumber+secondNumber}`)
-alert(`${firstNumber} - ${secondNumber} = ${firstNumber-secondNumber}`)
-alert(`${firstNumber} * ${secondNumber} = ${firstNumber*secondNumber}`)
-alert(`${firstNumber} / ${secondNumber} = ${firstNumber/secondNumber}`)
+alert(numberOfSeconds)
