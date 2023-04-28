02bb6260c3de6eb8618fc55fc6da3267f8136327
Vlad <vladus007445@gmail.com>
2023-04-28 16:25:03 +0300

    home work #13

--- index.js
+++ index.js
@@ -1,95 +1,17 @@
-let users = [
-   {
-   "index": 0,
-   "isActive": true,
-   "balance": "$2,226.60",
-   "name": "Eugenia Sawyer",
-   "gender": "female",
-   "phone": "+1 (840) 583-3207",
-   "address": "949 John Street, Rose, Puerto Rico, 1857"
+let ladder = {
+   step: 0,
+   up: function () {
+       this.step++;
+       return this;
    },
-   {
-   "index": 1,
-   "isActive": true,
-   "balance": "$2,613.77",
-   "name": "Pauline Gallegos",
-   "gender": "female",
-   "phone": "+1 (985) 593-3328",
-   "address": "328 Greenpoint Avenue, Torboy, North Dakota, 6857"
+   down: function () {
+       this.step--;
+       return this;
    },
-   {
-   "index": 2,
-   "isActive": false,
-   "balance": "$3,976.41",
-   "name": "Middleton Chaney",
-   "gender": "male",
-   "phone": "+1 (995) 591-2478",
-   "address": "807 Fleet Walk, Brutus, Arkansas, 9783"
-   },
-   {
-   "index": 3,
-   "isActive": true,
-   "balance": "$1,934.58",
-   "name": "Burns Poole",
-   "gender": "male",
-   "phone": "+1 (885) 559-3422",
-   "address": "730 Seba Avenue, Osage, Alabama, 6290"
-   },
-   {
-   "index": 4,
-   "isActive": true,
-   "balance": "$3,261.65",
-   "name": "Mcfadden Horne",
-   "gender": "male",
-   "phone": "+1 (942) 565-3988",
-   "address": "120 Scholes Street, Kirk, Michigan, 1018"
-   },
-   {
-   "index": 5,
-   "isActive": false,
-   "balance": "$1,790.56",
-   "name": "Suzette Lewis",
-   "gender": "female",
-   "phone": "+1 (837) 586-3283",
-   "address": "314 Dunne Place, Bawcomville, Guam, 9053"
+   showStep: function () { 
+       alert(this.step);
+       return this;
    }
-   ]
+};
 
-   // Функция приведения баланса в Number 
-   
-   function filterOfBalance (balance) {
-      let filterBalanceArray = [];
-      let balanceArray = balance.split("");
-      for (let i = 0; i < balanceArray.length; i++) {
-         if(balanceArray[i] !== '$' && balanceArray[i] !== ','){
-            filterBalanceArray.push(balanceArray[i])
-         }
-      }
-      const readyBalance = filterBalanceArray.join('')
-
-      return Number(readyBalance);
-   }
-
-   // Функция подсчета суммы и вивода массива номеров  
-
-   function filterOfArray (array) {
-      let arrayOfNumbers = [];
-      let sum = 0;
-
-      array.forEach(element => {
-          let balance = filterOfBalance(element.balance)
-          sum += balance;
-
-          if(balance > 2000) {
-            arrayOfNumbers.push(element.phone)
-          }
-      });
-
-       sum = Math.round(sum)
-      
-       console.log('Массив - ', arrayOfNumbers);
-       console.log(`Сумма - ${sum} $`);
-       
-   }
-
-    filterOfArray(users);
+ladder.up().up().down().showStep();
