7a67d035d180236e2f4525595d0da5d3536bd7a9
Vlad <vladus007445@gmail.com>
2023-05-01 23:11:19 +0300

    home work #14

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
-let ladder = {
-   step: 0,
-   up: function () {
-       this.step++;
-       return this;
-   },
-   down: function () {
-       this.step--;
-       return this;
-   },
-   showStep: function () { 
-       alert(this.step);
-       return this;
-   }
-};
+class Human {
+    constructor(name,gender) {
+        this.name = name;
+        this.gender = gender;
+    }
+}
 
-ladder.up().up().down().showStep();
+class Flat {
+    residents = [];
+
+    addResidents(obj) {
+        this.residents.push(obj)
+        return this.residents
+    }
+}
+
+class House {
+
+    arrayOfFlets = [];
+
+    constructor(numOfFlats) {
+        this.numOfFlats = numOfFlats;
+    }
+
+    addFlat(flet){
+        if(this.numOfFlats <= this.arrayOfFlets.length){
+            console.log('Количество квартир в этом доме достигло максимального значения')
+        }else{
+            this.arrayOfFlets.push(flet)
+        }
+    }
+
+}
+
+const Vlad = new Human('Vlad', 'male');
+const Danil = new Human('Danil', 'male');
+const Vika = new Human('Vika', 'female');
+const Masha = new Human('Masha', 'female');
+
+const Flat1 = new Flat();
+const Flat2 = new Flat();
+
+Flat1.addResidents(Danil)
+Flat1.addResidents(Vika)
+Flat1.addResidents(Masha)
+
+Flat2.addResidents(Vlad)
+Flat2.addResidents(Masha)
+
+const houseNumber1 = new House(2);
+const houseNumber2 = new House(1);
+
+houseNumber1.addFlat(Flat1)
+houseNumber1.addFlat(Flat2)
+
+houseNumber2.addFlat(Flat1)
+houseNumber2.addFlat(Flat2)
+
+
+
