6cc3d512b8f86b00e5e002187bdd6b902f7b2ac2
Vlad <vladus007445@gmail.com>
2023-05-16 02:15:20 +0300

    home work #19

--- index.js
+++ index.js
@@ -1,44 +1,11 @@
 const root = document.getElementById('root');
+const img = document.createElement('img');
 
-const arrayOfNumbers = [];
-
-let numOfTd = 100;
-
-for (let i = 1; i <= numOfTd; i++) {
-   arrayOfNumbers.push(i);
+let randomNumber = Math.floor(Math.random() * 10)
+if(randomNumber === 0) {
+   randomNumber = Math.floor(Math.random() * 10)
 }
 
+img.src = `./images/${randomNumber}.jpg`
 
-function createTable(parent, cols, rows) {
-	let table = document.createElement('table');
-	
-	for (let i = 0; i < rows; i++) {
-		let tr = document.createElement('tr');
-		
-		for (let j = 0; j < cols; j++) {
-			let td = document.createElement('td');
-         td.style.textAlign = 'center';
-         td.style.width = '30px';
-         td.style.height = '30px';
-         td.style.border = '1px solid black';
-			tr.appendChild(td);
-		}
-		
-		table.appendChild(tr);
-	}
-	
-	parent.appendChild(table);
-}
-
-createTable(root, 10, 10);
-
-const init = document.getElementsByTagName('td');
-
-const initArr = Array.from(init);
-
-let counter = 0
-
-for (let i = 0; i < arrayOfNumbers.length; i++){
-   initArr[counter].textContent = arrayOfNumbers[counter];
-   counter++;
-}
+root.append(img);
