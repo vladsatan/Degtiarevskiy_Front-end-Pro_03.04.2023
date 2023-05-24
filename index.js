5bc5927ec9aa0cb9475e1a30e7217aa8b69b8242
Vlad <vladus007445@gmail.com>
2023-05-25 00:58:45 +0300

    home work #22

--- index.js
+++ index.js
@@ -1,22 +1,51 @@
-const link = document.getElementById('link').textContent;
-const http = document.getElementById('http');
-const https = document.getElementById('https');
+const slider = document.getElementById('slider');
+const images = slider.getElementsByTagName('img');
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+let currentIndex = 0;
 
-function forwarder(protocol){
-
-    let linkProto = link.slice(0, 4)
-
-    if(linkProto === 'http'){
-      location.href = link
-   }else {
-      if(protocol === 'https'){
-         window.location.href = 'https://' + link
-      }
-      if(protocol === 'http'){
-         window.location.href = 'http://' + link
+function showImage(index) {
+   for (let i = 0; i < images.length; i++) {
+      if (i === index) {
+         images[i].style.display = 'block';
+      } else {
+         images[i].style.display = 'none';
       }
    }
 }
 
-http.addEventListener('click', ()=> forwarder('http'))
-https.addEventListener('click', ()=> forwarder('https'))
+function updateButtons() {
+   if (currentIndex === 0) {
+      prevBtn.style.display = 'none';
+   } else {
+      prevBtn.style.display = 'block';
+   }
+
+   if (currentIndex === images.length - 1) {
+      nextBtn.style.display = 'none';
+   } else {
+      nextBtn.style.display = 'block';
+   }
+}
+
+function showNextImage() {
+   if (currentIndex < images.length - 1) {
+      currentIndex++;
+      showImage(currentIndex);
+      updateButtons();
+   }
+}
+
+function showPrevImage() {
+   if (currentIndex > 0) {
+      currentIndex--;
+      showImage(currentIndex);
+      updateButtons();
+   }
+}
+
+nextBtn.addEventListener('click', showNextImage);
+prevBtn.addEventListener('click', showPrevImage);
+
+showImage(currentIndex);
+updateButtons();
